ae18343cb7ab050176499219d523980634b12e63
loretta-dhahabu <loreydjefwa@gmail.com>
2022-06-06 22:27:06 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+
+const multiply =  num1 * num2;
+
+
+const random = +1;
+
+const num3 = 39;
+const num4 = 5;
+const mod = num3 % num4;
+
+const max = Math.max(1, 2, 4, 5, 10, 20,12,8);
